c457a299cd5508d7df7ac47c664b9e00a0c22847
a <myhmmngbrd@gmail.com>
2020-07-03 17:22:22 +0900

    순항중...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,88 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <vector>
+#include <string>
 
 #define WIDTH 10
 #define HEIGHT 20
 
+class Block {
+	int box[4][4] = { 0, };
+	int x=0, y=0;
+	char type;
+public:
+	Block(char type = 0) : type(type) {
+		switch (type)
+		{
+		default:
+			box[1][1] = 1;
+			box[1][2] = 1;
+			box[2][1] = 1;
+			box[2][2] = 1;
+			break;
+		}
+	}
+	void init() {
+		x = 5;
+		y = 1;
+	}
+	void left() {
+		x--;
+	}
+	void right() {
+		x++;
+	}
+	void down() {
+		y++;
+	}
+	int ver() {
+		return y;
+	}
+	int hor() {
+		return x;
+	}
+	int* operator[] (int index) {
+		return box[index];
+	}
+};
+
+class Board {
+	int box[20][10] = { 0, };
+public:
+	int before[20][10] = { 0, };
+	int index = 0;
+	void backup() {
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+				before[i][j] = box[i][j];
+			}
+		}
+	}
+
+	void shift(int index) {
+		if (index >= 20) exit(1);
+		for (int i = index; index >= 0; i--) {
+			for (int j = 0; j < 10; j++) {
+				box[i][j] = box[i - 1][j];
+			}
+		}
+	}
+	void print() {
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				printf("%d ", box[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	int* operator[] (int index) {
+		return box[index];
+	}
+};
+
 class Game {
 	Board board;
+	Block block;
  public:
 	Game() {
 		_COORD coord;
@@ -22,6 +98,7 @@ class Game {
 		HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleScreenBufferSize(Handle, coord);
 		SetConsoleWindowInfo(Handle, TRUE, &Rect);
+
 	}
 	
 	void gotoxy(int x, int y) {
@@ -65,76 +142,50 @@ class Game {
 	void start() {
 		drawbox(0, 0, 10, 20);
 		drawbox(12, 0, 4, 2);
+		update();
+		update();
+		update();
+		update();
+		gotoxy(25, 25);
 	}
 	void update() {
-
-	}
-};
-
-class Board {
-	int box[20][10] = { 0, };
-public:
-	void fall() {
-
-	}
-
-	void rotate() {
-
-	}
-
-	void shift(int index) {
-		if (index >= 20) exit(1);
-		for (int i = index; index >= 0; i--) {
-			for (int j = 0; j < 10; j++) {
-				box[i][j] = box[i - 1][j];
-			}
-		}
-	}
-	void print() {
+		Sleep(500);
+		board[0][board.index++] = 1;
+		if (board.index > 2) board[0][board.index - 2] = 0;
 		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				printf("%d ", box[i][j]);
+			for (int j = 0; j < 10; j++) {
+				if (board[i][j] != board.before[i][j]) {
+					gotoxy(j * 2 + 2, i + 1);
+					board[i][j] ? printf("■") : printf("  ");
+				}
 			}
-			printf("\n");
 		}
+		board.backup();
 	}
-	int& operator[] (int index) {
-		return box[index];
+	void CreateBlock() {
+		block.init();
+
+	}
+	int InputBlock() {
+		int collision = CheckCollision();
+		if (collision) {
+			return 0;
+		} else 
+	}
+	int CheckCollision() {
+		int x = block.hor();
+		int y = block.ver();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (!block[i][j]) continue;
+				if (board[y - 1 + i][x - 1 + j] > 0) { // 충돌
+					return 1;
+				}
+			}
+		}
+		return 0;
 	}
 };
-
-class Block {
-	char type;
-	int x, y, r;
-	std::vector<int> box;
-public:
-	void create(char type) {
-		switch (type)
-		{
-		case 'I':
-			box = { 0, -2, 0, -1, 0, 0, 0, -1 };
-			break;
-		case 'O':
-			box = { 0, 0, 0, 1, 1, 1, 1, 0 };
-		case 'L':
-			
-			break;
-		case 'J':
-			break;
-		case 'Z':
-			break;
-		case 'S':
-			break;
-		case 'T':
-			break;
-		default:
-			break;
-		}
-	}
-};
-
-
-
 
 void cursorview(char show)//커서숨기기
 {
