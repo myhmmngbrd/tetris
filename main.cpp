9755b709801e7ea7eb88704d16c181d016b22c08
a <myhmmngbrd@gmail.com>
2020-07-10 17:12:31 +0900

    기능적인 부분은 거의 구현, 최적화 필요

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include <thread>
 #include <conio.h>
 #include <random>
-#include <future>
-#include <functional>
 #include <mutex>
+#include <future>
+#include <chrono>
 
 #define I_BLOCK 0
 #define T_BLOCK 1
@@ -25,6 +25,10 @@
 #define ANSI_MAGENTA "\x1b[35m"
 #define ANSI_CYAN "\x1b[36m"
 #define ANSI_RESET "\x1b[0m"
+
+#define STANDBY 0
+#define START 1
+#define FALLING 2
 
 using std::array;
 using std::vector;
@@ -217,7 +221,18 @@ public:
 	void left() { x--; }
 	void right() { x++; }
 	void rotate() {
-		if (++rotation = blocks[type].size()) rotation = 0;
+		if (++rotation == blocks[type].size()) rotation = 0;
+		updateblock();
+	}
+	void setx(int x) { this->x = x; }
+	void sety(int y) { this->y = y; }
+	void clear() {
+		block = { 0, };
+		oldblock = { 0, };
+		x = 0;
+		y = 0;
+		rotation = 0;
+		log = { 0, };
 	}
 
 	auto& getoldblock() { return oldblock; }
@@ -270,6 +285,22 @@ public:
 		return collisions;
 	}
 
+	void clear(int index) {
+		for (int i = index; i > 0; i--) {
+			board[i] = board[i - 1];
+		}
+	}
+	void checkclear() {
+		int full;
+		for (int i = 0; i < 20; i++) {
+			full = 1;
+			for (int j = 0; j < 10; j++) {
+				if (!board[i][j]) full = 0;
+			}
+			if (full) clear(i);
+		}
+	}
+
 	void print() {
 		system("cls");
 		for (auto row : board) {
@@ -285,10 +316,6 @@ public:
 			}
 			printf("\n");
 		}
-		auto c = compare();
-		for (auto e : c) {
-			printf(" %d ", e);
-		}
 	}
 
 	auto& operator[] (int index) {
@@ -301,11 +328,13 @@ class Game {
 	Block illusion;
 	Board board;
 	Canvas canvas = Canvas(0, 0, 50, 30);
+	std::mutex m;
+	std::condition_variable cv;
+	int moveable = 0;
 	std::random_device rd;
-
 	std::mt19937 gen;
-
 	std::uniform_int_distribution<int> dis;
+	int step = STANDBY;
 public:
 	Game() {
 		gen = std::mt19937(rd());
@@ -320,10 +349,10 @@ public:
 		int y = block.gety();
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (block[i][j] && i + y >= 0) {
+				if (block[i][j]) {
 					if (i + y >= 20) return 1; // 바닥에 충돌
 					else if (j + x >= 10 || j + x < 0) return 2; // 벽면에 충돌
-					else if (board[i + y][j + x] && board[i + y][j + x] != 8) { return 3; } // 자신의 이전 상태가 아닌 벽돌과 충돌, 
+					else if (i + y >= 0 && board[i + y][j + x] && board[i + y][j + x] != 8) { return 3; } // 자신의 이전 상태가 아닌 벽돌과 충돌, 
 				}
 			}
 		}
@@ -347,7 +376,7 @@ public:
 		int x = block.getoldx(), y = block.getoldy();
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (oldblock[i][j] && i + y < 20 && i + y >= 0 && j + x < 20 && j + x >= 0) {
+				if (oldblock[i][j] && i + y < 20 && i + y >= 0 && j + x < 10 && j + x >= 0) {
 					if (oldblock[i][j] == board[i + y][j + x]) {
 						board[i + y][j + x] = 0;
 					}
@@ -360,14 +389,14 @@ public:
 		int x = block.getoldx(), y = block.getoldy();
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (oldblock[i][j] && i + y < 20 && i + y >= 0 && j + x < 20 && j + x >= 0) {
+				if (oldblock[i][j] && i + y < 20 && i + y >= 0 && j + x < 10 && j + x >= 0) {
 					board[i + y][j + x] = oldblock[i][j];
 				}
 			}
 		}
 	}
 
-	void createblock(Block& block) {
+	int createblock(Block& block) {
 		block.init(dis(gen), dis(gen) + 1);
 		block.backupblock();
 		block.backupx();
@@ -379,21 +408,26 @@ public:
 			block.backupx();
 			block.backupy();
 			block.backuprotation();
+			return 1;
+		}
+		else {
+			return 0;
 		}
 	}
-
-	void lowerblock(Block& block) {
+	int lowerblock(Block& block) {
 		block.down();
 		int collision = compare(block);
 		if (!collision) {
 			input(block);
 			block.backupy();
+			return 1;
 		} else {
 			block.reverty();
+			reinput(block);
+			return 0;
 		}
 	}
-
-	void dropblock(Block& block) {
+	int dropblock(Block& block) {
 		block.down();
 		int collision = compare(block);
 		if (!collision) {
@@ -404,37 +438,50 @@ public:
 			if (!collision) {
 				input(block);
 				block.backupy();
-				return;
+				return 1;
 			} else {
-				block.reverty();
-				return;
+				return 0;
 			}
 		}
 	}
-
-	void moveleftblock(Block& block) {
+	int moveleftblock(Block& block) {
 		block.left();
 		int collision = compare(block);
 		if (!collision) {
 			input(block);
 			block.backupx();
-			return;
+			return 1;
 		} else {
 			block.revertx();
-			return;
+			return 0;
 		}
 	}
-	void moverightblock(Block& block) {
+	int moverightblock(Block& block) {
 		block.right();
 		int collision = compare(block);
 		if (!collision) {
 			input(block);
 			block.backupx();
-			return;
+			return 1;
 		}
 		else {
 			block.revertx();
-			return;
+			return 0;
+		}
+	}
+	int rotateblock(Block& block) {
+		block.rotate();
+		int collision = compare(block);
+		if (!collision) {
+			input(block);
+			block.backupblock();
+			block.backuprotation();
+			return 1;
+		}
+		else {
+			block.revertblock();
+			block.revertrotation();
+			return 0;
 		}
 	}
 
@@ -465,460 +512,158 @@ public:
 		}
 		canvas.DrawDots(dots);
 	}
-
-	void start() {
-		Sleep(500);
-		createblock(block);
-		remove(block);
-		illusion.init(block.gettype(), 8);
-		illusion.backupblock();
-		illusion.backupx();
-		illusion.backupy();
-		dropblock(illusion);
-		reinput(block);
-		view();
-		//board.print();
-		board.backupboard();
-		Sleep(500);
-		lowerblock(block);
-		view();
-		//board.print();
-		board.backupboard();
-		Sleep(500);
-		lowerblock(block);
-		view();
-		//board.print();
-		board.backupboard();
-		Sleep(500);
-		dropblock(block);
-		view();
-		//board.print();
-		board.backupboard();
-
-		Sleep(500);
-		createblock(block);
-		remove(block);
-		illusion.init(block.gettype(), 8);
-		illusion.backupblock();
-		illusion.backupx();
-		illusion.backupy();
-		dropblock(illusion);
-		reinput(block);
-		view();
-
-	}
-
-};
-
-
-/*
-
-class Block {
-	array< array<int, 4>, 4 > box = { 0, };
-	array< array<int, 4>, 4 > before = { 0, };
-	array<int, 2> log;
-	int x = 0, y = 0; // box[0][1] = {x:1, y:0}
-public:
-	void backup() {
-		log[0] = x;
-		log[1] = y;
-	}
-	void cancel() {
-		x = log[0];
-		y = log[1];
-	}
-	void print() {
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				printf("%d ", box[i][j]);
-			}
-			printf("\n");
-		}
-	}
-	array<int, 4>& operator[] (int index) {
-		return box[index];
-	}
-
-
-	void init(int  type) {
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				box[i][j] = 0;
-			}
-		}
-		switch (type)
-		{
-		case I_BLOCK:
-			box[2][0] = 1;
-			box[2][1] = 1;
-			box[2][2] = 1;
-			box[2][3] = 1;
-			break;
-		case O_BLOCK:
-			box[1][1] = 1;
-			box[1][2] = 1;
-			box[2][1] = 1;
-			box[2][2] = 1;
-			break;
-		case L_BLOCK:
-			box[1][1] = 1;
-			box[2][1] = 1;
-			box[2][2] = 1;
-			box[2][3] = 1;
-			break;
-		case J_BLOCK:
-			box[1][2] = 1;
-			box[2][2] = 1;
-			box[2][1] = 1;
-			box[2][0] = 1;
-			break;
-		case S_BLOCK:
-			box[2][0] = 1;
-			box[2][1] = 1;
-			box[1][1] = 1;
-			box[1][2] = 1;
-			break;
-		case Z_BLOCK:
-			box[2][3] = 1;
-			box[2][2] = 1;
-			box[1][2] = 1;
-			box[1][1] = 1;
-			break;
-		case T_BLOCK:
-			box[1][1] = 1;
-			box[2][0] = 1;
-			box[2][1] = 1;
-			box[2][2] = 1;
-			break;
-		default:
-			break;
-		}
-		x = 3;
-		y = -2;
-	}
-	void down() {
-		y++;
-	}
-	void left() {
-		x--;
-	}
-	void right() {
-		x++;
-	}
-	void rotate() {
-		int tmp[4][4];
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				before[i][j] = box[i][j];
-				tmp[i][j] = box[j][3 - i];
-			}
-		}
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				box[i][j] = tmp[i][j];
-			}
-		}
-	}
-	void rollback() {
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				box[i][j] = before[i][j];
-			}
-		}
-	}
-	int hor() {
-		return x;
-	}
-	int ver() {
-		return y;
-	}
-};
-
-class Board {
-	array<array<int, 10>, 20> box = { 0, };
-public:
-	array<array<int, 10>, 20> log = { 0, };
-	void print() {
-		system("cls");
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				printf("%d ", box[i][j]);
-			}
-			printf("\n");
-		}
-	}
-	array<int, 10>& operator[] (int index) {
-		return box[index];
-	}
-	void backup() {
-		log = box;
-	}
-	void RemoveBlock() {
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (box[i][j] == 1) box[i][j] = 0;
-			}
-		}
-	}
-	void RemoveRow(int index) {
-		for (int i = index; i > 0; i--) {
-			box[i] = box[i - 1];
-		}
-	}
-	void CheckFull () {
-		int hole = 1;
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (!box[i][j]) hole = 1;
-			}
-			if (hole) hole = 0;
-			else RemoveRow(i);
-		}
-	}
-	void solid() {
-		CheckFull();
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (box[i][j]) box[i][j] = 2;
-			}
-		}
-	}
-};
-
-class Game {
-	Block block;
-	Board board;
-	std::mutex m;
-	void message(string m) {
-		gotoxy(30, 10);
-		std::cout << m << std::endl;
-	}
-	void cursorview(char show)//커서숨기기
-	{
-		HANDLE hConsole;
-		CONSOLE_CURSOR_INFO ConsoleCursor;
-
-		hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-		ConsoleCursor.bVisible = show;
-		ConsoleCursor.dwSize = 1;
-
-		SetConsoleCursorInfo(hConsole, &ConsoleCursor);
-	}
-	void gotoxy(int x, int y) {
-		COORD Pos;
-		Pos.X = x;
-		Pos.Y = y;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-	}
-	void drawbox(int x, int y, int w, int h) {
-		
-		* 너비와 높이는 박스가 아닌 컨텐츠기준
-		* 좌상: ┌
-		* 좌하: └
-		* 우상: ┐
-		* 우하: ┘
-		* 가로: ─
-		* 세로: │
-		* 박스: □ ■
-		
-		gotoxy(x * 2, y);
-		printf("┌");
-		gotoxy(x * 2 + w * 2 + 2, y);
-		printf("┐");
-		gotoxy(x * 2, y + h + 1);
-		printf("└");
-		gotoxy(x * 2 + w * 2 + 2, y + h + 1);
-		printf("┘");
-		for (int i = 2; i <= w * 2; i = i + 2) {
-			gotoxy(x * 2 + i, y);
-			printf("─");
-			gotoxy(x * 2 + i, y + h + 1);
-			printf("─");
-		}
-		for (int i = 1; i <= h; i++) {
-			gotoxy(x * 2, y + i);
-			printf("│");
-			gotoxy(x * 2 + w * 2 + 2, y + i);
-			printf("│");
-		}
-	}
-	void Input() { // 보드에 블럭 집어넣기
-		board.RemoveBlock();
-		int x = block.hor();
-		int y = block.ver();
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (block[i][j] && (y + i >= 0) && (x + j >= 0)) {
-					board[y + i][x + j] = 1;
-				}
-			}
-		}
-	}
-	int Create(int type) {
-		block.backup();
-		block.init(type);
-		std::vector<int> crash = CrashCheck();
-
-		if (crash.size()) { //충돌하면
-			return 0; //게임종료
-		}
-		else {
-			board.backup();
-			Input();
-			return 1;
-		}
-	}
-	int down() {
-		block.backup();
-		board.backup();
-		block.down();
-		std::vector<int> crash = CrashCheck();
-		if (!crash.size()) { // 충돌 없으면
-			Input();
-			return 1;
-		}
-		else { // 있으면
-			block.cancel();
-			board.solid();
+	int create() {
+		int success = createblock(block);
+		if (!success) {
+			this->step = STANDBY;
 			return 0;
 		}
+		remove(block);
+		illusion.init(block.gettype(), 8);
+		illusion.backupblock();
+		illusion.backupx();
+		illusion.backupy();
+		dropblock(illusion);
+		reinput(block);
+		view();
+		//board.print();
+		board.backupboard();
+		this->step = FALLING;
+		this->moveable = 1;
+		return 1;
 	}
-	void left() {
-		block.backup();
-		block.left();
-		std::vector<int> crash = CrashCheck();
-		if (!crash.size()) {
-			board.backup();
-			Input();
-		} else block.cancel();
-	}
-	void right() {
-		block.backup();
-		block.right();
-		std::vector<int> crash = CrashCheck();
-		if (!crash.size()) {
-			board.backup();
-			Input();
-		} else block.cancel();
-	}
-	void fall() {
-		block.backup();
-		block.down();
-		std::vector<int> crash = CrashCheck();
-		if (!crash.size()) {
-			return fall();
-		} else {
-			board.backup();
-			block.cancel();
-			Input();
-			board.solid();
-			return;
+	int down() {
+		int success = lowerblock(block);
+		if (!success) {
+			this->step = START;
+			this->moveable = 0;
+			board.backupboard();
+			board.checkclear();
+			view();
+			return 0;
 		}
+		view();
+		//board.print();
+		board.backupboard();
+		this->step = FALLING;
+		return 1;
 	}
-	void rotate() {
-		block.rotate();
-		std::vector<int> crash = CrashCheck();
-		if (!crash.size()) {
-			board.backup();
-			Input();
+	int fall() {
+		int success = dropblock(block);
+		if (!success) {
+			board.backupboard();
+			return 0;
 		}
-		else block.rollback();
+		board.checkclear();
+		view();
+		//board.print();
+		board.backupboard();
+		return 1;
 	}
-	std::vector<int> CrashCheck() {
-		int x = block.hor();
-		int y = block.ver();
-		std::vector<int> result;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (block[i][j] && ((i + y >= 20) || (j + x < 0) || (j + x >= 10))) {
-					result = { -1 };
-					break;
-				}
-				if (block[i][j] && (i + y >= 0) && (j + x >= 0) && (board[i + y][j + x] == 2)) result.push_back(4 * i + j);
-			}
-		}
-		return result;
-	}
-	void Update() {
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (board[i][j] != board.log[i][j]) {
-					gotoxy(j * 2 + 2, i + 1);
-					if (board[i][j] == 1) {
-						printf("■");
-					}
-					else if (board[i][j] == 2) {
-						printf("□");
-					}
-					else if (!board[i][j]) {
-						printf("  ");
 
-					}
-				}
-			}
-		}
+	int left() {
+		int success = moveleftblock(block);
+		if (!success) return 0;
+		remove(block);
+		illusion.sety(block.gety());
+		moveleftblock(illusion);
+		dropblock(illusion);
+		reinput(block);
+		view();
+		//board.print();
+		board.backupboard();
+		return 1;
 	}
-	void KeyDown() {
+
+	int right() {
+		int success = moverightblock(block);
+		if (!success) return 0;
+		remove(block);
+		illusion.sety(block.gety());
+		moverightblock(illusion);
+		dropblock(illusion);
+		reinput(block);
+		view();
+		//board.print();
+		board.backupboard();
+		return 1;
+	}
+
+	int rotate() {
+		int success = rotateblock(block);
+		if (!success) return 0;
+		remove(block);
+		illusion.sety(block.gety());
+		rotateblock(illusion);
+		dropblock(illusion);
+		reinput(block);
+		view();
+		//board.print();
+		board.backupboard();
+		return 1;
+	}
+
+	void keyboard() {
 		while (1) {
 			// 75 left
 			// 77 right
 			// 80 down
 			// 32 space
 			// 72 up
-			int key = _getch();
-			if (key) {
+			int key = 0;
+			key = _getch();
+			if (this->moveable && key) {
 				m.lock();
-				std::vector<int> crash;
 				if (key == 75) {
 					left();
-				} else if (key == 77) {
+				}
+				else if (key == 77) {
 					right();
-				} else if (key == 80) {
-					down();
-				} else if (key == 32) {
+				}
+				else if (key == 80) {
+					if (!down()) {
+						this->step = START;
+						this->moveable = 0;
+					}
+				}
+				else if (key == 32) {
+					this->step = START;
+					this->moveable = 0;
 					fall();
-				} else if (key == 72) {
+					cv.notify_one();
+				}
+				else if (key == 72) {
 					rotate();
 				}
-				Update();
 				m.unlock();
 			}
-		}
+		};
 	}
-public:
+
 	void start() {
-		std::random_device rd;
-
-		std::mt19937 gen(rd());
-
-		std::uniform_int_distribution<int> dis(0, 6);
-
-		cursorview(0);
-		drawbox(0, 0, 10, 20);
-		std::thread t([this]() { KeyDown(); });
-		int gameover, landing;
+		std::thread t([this]() {keyboard(); });
+		std::unique_lock<std::mutex> lk(m);
+		
 		do {
 			Sleep(200);
-			m.lock();
-			gameover = Create(dis(gen));
-			gotoxy(30, 25);
-			std::cout << gameover;
-			Update();
-			m.unlock();
+			//m.lock();
+			create();
+			//m.unlock();
 			do {
-				Sleep(100);
-				m.lock();
-				landing = down();
-				Update();
-				m.unlock();
-			} while (landing);
-		} while (gameover);
+				if (cv.wait_for(lk, std::chrono::seconds(1), [&]() {return this->step == START; })) break;
+				if (!down()) {
+					this->step = START;
+					this->moveable = 0;
+				}
+			} while (this->step == FALLING);
+		} while (this->step == START);
+
 		t.join();
 	}
+
 };
 
-*/
+
 int main() {
 	Game game;
 	game.start();
